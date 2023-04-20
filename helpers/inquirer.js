876f369f2a68800b929f0fa4fe33f965cab4085b
David B <david.banol.3889@eam.edu.co>
2023-04-20 16:31:19 -0500

    Mejora en listado ciudades

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -70,13 +70,13 @@ const leerInput = async( message ) => {
     return desc;
 }
 
-const listadoTareasBorrar = async( tareas = [] ) => {
-    const choices = tareas.map( (tarea, i) =>  {
+const listarLugares = async( lugares = [] ) => {
+    const choices = lugares.map( (lugar, i) =>  {
 
         const idx = `${ i + 1 }.`.green.bold;
         return {
-            value: tarea.id,
-            name: `${ idx } ${ tarea.desc }`
+            value: lugar.id,
+            name: `${ idx } ${ lugar.nombre }`
         }
     });
 
@@ -89,7 +89,7 @@ const listadoTareasBorrar = async( tareas = [] ) => {
         {
             type: 'list',
             name: 'id',
-            message: 'Borrar',
+            message: 'Seleccione por favor el lugar',
             choices
         }
     ]
@@ -143,9 +143,9 @@ module.exports = {
     inquirerMenu,
     pausa,
     leerInput,
-    listadoTareasBorrar,
     confirmar,
     mostrarListadoChecklist,
+    listarLugares
 }
 
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { leerInput, inquirerMenu, pausa} = require("./helpers/inquirer");
+const { leerInput, inquirerMenu, pausa, listarLugares} = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
 const main = async() => {
@@ -14,19 +14,21 @@ const main = async() => {
         switch (opt) {
             case 1:
                 //Mostrar mensaje
-                const lugar = await leerInput('Ciudad: ');
-                await busquedas.ciudad(lugar);
+                const termino = await leerInput('Ciudad: ');
 
+                //Buscar lugares
+                const lugares = await busquedas.ciudad(termino);
 
+                //Seleccionar el lugar
+                const idSeleccionado = await listarLugares(lugares);
+                const lugarSel = lugares.find( l => l.id === idSeleccionado);
 
-                //TODO Buscar lugares
-                //TODO Seleccionar el lugar
                 //TODO Datos clima
                 //TODO Mostrar resultados
                 console.log('\Información de la ciudad\n'.cyan.underline.bold);
-                console.log('ciudad:',);
-                console.log('lat:',);
-                console.log('lng:',);
+                console.log('ciudad:', lugarSel.nombre);
+                console.log('lat:', lugarSel.lat);
+                console.log('lng:', lugarSel.lng);
                 console.log('temperatura:',);
                 console.log('mínima:',);
                 console.log('máxima:',);
