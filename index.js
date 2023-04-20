aa2110ea3fde1805fd6d36ef99be606697f540e9
David B <david.banol.3889@eam.edu.co>
2023-04-20 15:05:45 -0500

    Primer commit-modelo app

---
+++ index.js
@@ -0,0 +1,43 @@
+const { leerInput, 
+        inquirerMenu, 
+        pausa
+    } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
+
+
+
+const main = async() => {
+
+    const busquqedas = new Busquedas();
+    let opt;
+
+    do{
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+                //TODO Buscar lugares
+                //TODO Seleccionar el lugar
+                //TODO Datos clima
+                //TODO Mostrar resultados
+                console.log('\Información de la ciudad\n'.cyan.underline.bold);
+                console.log('ciudad:',);
+                console.log('lat:',);
+                console.log('lng:',);
+                console.log('temperatura:',);
+                console.log('mínima:',);
+                console.log('máxima:',);
+
+            break;
+
+        }
+
+        if ( opt !== 0 ) await pausa();
+
+    } while ( opt !== 0 );
+}
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,21 @@
+
+class Busquedas {
+
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor() {
+        //TODO Implementar leer DB si existe
+    }
+
+    async ciudad ( lugar = ''){
+        //Peticion http
+
+        console.log(lugar);
+
+        return []; //Retornar los lugares
+    }
+}
+
+
+
+module.exports = Busquedas;
